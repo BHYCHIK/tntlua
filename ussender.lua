1be9181f8e6e97a0726687afcf4eceb0573f58a5
Ivan Remen <i.remen@corp.mail.ru>
2017-04-10 07:08:47 -0400

    Ussender clear

--- ussender.lua
+++ ussender.lua
@@ -1,12 +1,28 @@
 -- Race conditions in this files are possible. It is ok by biz logic.
+--
+
+local tuple_length_limit = 1001 --max numbers of element + 1. VALUE MUST BE ODD.
+
+local function truncate_tuple(selected)
+    selected = selected:transform(1, #selected - tuple_length_limit):transform(1, (tuple_length_limit - 1) / 2)
+    box.replace(0, selected)
+    return selected
+end
+
 function ussender_add(user_id, sender_id)
     local user_id = box.unpack("i", user_id)
     local sender_id = box.unpack("l", sender_id)
 
     local selected = { box.select_limit(0, 0, 0, 1, user_id) }
+
     if #selected == 0 then
         box.insert(0, user_id, sender_id)
     else
+
+        if #selected[1] >= tuple_length_limit then
+            selected[1] = truncate_tuple(selected[1])
+        end
+
         local fun, param, state = selected[1]:pairs()
         state, _ = fun(param, state) -- skip the first element of tuple
         for state, v in fun, param, state do
@@ -25,6 +41,9 @@ function ussender_select(user_id)
     local ret = {box.select_limit(0, 0, 0, 1, user_id)}
     if #ret == 0 then
         return {user_id}
+    end
+    if #ret[1] >= tuple_length_limit then
+        ret[1] = truncate_tuple(ret[1])
     end
     return ret
 end
