f4e5f09c9ecb772bdd2120c7d03a9023d063316d
Ivan Remen <i.remen@corp.mail.ru>
2017-04-10 07:39:11 -0400

    space number unhardcode

--- ussender.lua
+++ ussender.lua
@@ -2,10 +2,11 @@
 --
 
 local tuple_length_limit = 1001 --max numbers of element + 1. VALUE MUST BE ODD.
+local space_no = 0
 
 local function truncate_tuple(selected)
     selected = selected:transform(1, #selected - tuple_length_limit):transform(1, (tuple_length_limit - 1) / 2)
-    box.replace(0, selected)
+    box.replace(space_no, selected)
     return selected
 end
 
@@ -13,7 +14,7 @@ function ussender_add(user_id, sender_id)
     local user_id = box.unpack("i", user_id)
     local sender_id = box.unpack("l", sender_id)
 
-    local selected = { box.select_limit(0, 0, 0, 1, user_id) }
+    local selected = { box.select_limit(space_no, 0, 0, 1, user_id) }
 
     if #selected == 0 then
         box.insert(0, user_id, sender_id)
@@ -31,14 +32,14 @@ function ussender_add(user_id, sender_id)
                 return
             end
         end
-        box.update(0, user_id, "!p", -1, sender_id)
+        box.update(space_no, user_id, "!p", -1, sender_id)
     end
 
 end
 
 function ussender_select(user_id)
     local user_id = box.unpack("i", user_id)
-    local ret = {box.select_limit(0, 0, 0, 1, user_id)}
+    local ret = {box.select_limit(space_no, 0, 0, 1, user_id)}
     if #ret == 0 then
         return {user_id}
     end
@@ -50,5 +51,5 @@ end
 
 function ussender_delete(user_id)
     local user_id = box.unpack("i", user_id)
-    box.delete(0, user_id)
+    box.delete(space_no, user_id)
 end
